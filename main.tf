53f986eae647d0a1a5b9a12d8f0039e37ee3bdf6
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-25 12:31:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name      = aws_key_pair.ssh_key.key_name  # Connect SSH key to the instance
   tags = {
     Name = var.instance_name
   }
@@ -13,4 +14,9 @@ resource "aws_instance" "ubuntu" {
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh-key"  # Provide a unique name for the key pair
+  public_key = tls_private_key.ssh.public_key_openssh
 }
